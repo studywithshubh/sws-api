96be4f8613429708364da3a8556e8b872dce87d6
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-03-21 20:59:25 +0530

    simple server coded

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("INITIALIZING SWSAPI REPO");
+import express from 'express';
+import { PORT } from './config';
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/" , (req , res) => {
+    res.send("SWS NEW SERVER IS UP!!")
+})
+
+app.listen(PORT , () => {
+    console.log(`BACKEND IS HOSTED : ${PORT}`)
+});
