f07009bdd37761c9ec156ccccdae8fec54dffc22
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-03-28 08:17:24 +0530

    enabled cors for the frontend vercel url

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://swsfetest.vercel.app",
     credentials: true
 }));
 
