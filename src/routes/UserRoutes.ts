aafacef39b86db28cbf18eb40dd8c9eb73e2fb7d
Shubhashish Chakraborty <164302071+Shubhashish-Chakraborty@users.noreply.github.com>
2025-04-16 08:12:52 +0000

    added functionality to look unverified users

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { filter, forgotPassword, getAllUsers, getUserCourses, logout, me, passwordReset, session, signin, signup, verify_email } from "../controllers/UserController";
+import { filter, forgotPassword, get_unverified_users, getAllUsers, getUserCourses, logout, me, passwordReset, session, signin, signup, verify_email } from "../controllers/UserController";
 import { UserAuth } from "../middlewares/UserAuthentication";
 
 export const UserRouter = Router();
@@ -8,6 +8,7 @@ UserRouter.post("/signup" , signup);
 UserRouter.post("/signin" , signin);
 UserRouter.post("/logout" , logout)
 UserRouter.post("/filter-unverified" , filter);
+UserRouter.get("/unverified" , get_unverified_users);
 UserRouter.post("/verify-mail" , verify_email);
 UserRouter.get("/data" , getAllUsers);
 UserRouter.get("/me" , UserAuth , me); // PROTECTED ENDPOINT!!
